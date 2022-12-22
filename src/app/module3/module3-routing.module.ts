ecda474a94910ea0c476412c2eaeb52c00d5f448
Rohit Sharma <rohitgargi0293@gmail.com>
2022-12-22 18:32:32 +0530

    Create module3-routing.module.ts

---
+++ src/app/module3/module3-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Module3Component } from './module3.component';
+
+const routes: Routes = [{ path: '', component: Module3Component }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class Module3RoutingModule { }
