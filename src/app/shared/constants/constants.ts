7cb1bc3e31500ba8db9d3b7edcb8292c6ecb1525
Rohit Sharma <rohitgargi0293@gmail.com>
2022-12-22 18:37:33 +0530

    Create constants.ts

---
+++ src/app/shared/constants/constants.ts
@@ -0,0 +1,32 @@
+export const sampleData = {
+    products: [
+        {
+            "productId":1,
+            "name":"Product1"
+        },
+        {
+            "productId":2,
+            "name":"Product2"
+          
+        },
+        {
+            "productId":3,
+            "name":"Product3"
+        }
+    ],
+    users : [{
+        "userId":"1",
+        "name":"Rohit",
+        "locId":"l1"
+      },
+      {
+        "userId":"2",
+        "name":"Hardik",
+        "locId":"l2"
+      },
+      {
+        "userId":"3",
+        "name":"Rahul",
+        "locId":"l3"
+      }]
+}
